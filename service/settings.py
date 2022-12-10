18964b20cde2ae30a96efba9c4797184f6493cbf
kikikita <girmanik.nik@gmail.com>
2022-12-11 00:39:21 +0300

    little fix

--- service/settings.py
+++ service/settings.py
@@ -9,11 +9,14 @@ from pydantic import BaseSettings
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-zip_file = zipfile.ZipFile('service/data/knn/knn_bm25_item.zip', 'r')
-knn_model = dill.load(zip_file.open('knn_bm25_item.dill'))
-pop_model = dill.load(open('service/data/knn/pop_model_7.dill', 'rb'))
-users_list = pickle.load(open('service/data/knn/users_list.pickle', 'rb'))
-embeds_maps = pickle.load(open('service/data/lightFM/emb_maps.pickle', 'rb'))
+zip_knn = zipfile.ZipFile('service/data/knn/knn_bm25_item.zip', 'r')
+knn_model = dill.load(zip_knn.open('knn_bm25_item.dill'))
+zip_pop = zipfile.ZipFile('service/data/knn/pop_model_7.zip', 'r')
+pop_model = dill.load(zip_pop.open('pop_model_7.dill'))
+us_zip = zipfile.ZipFile('service/data/knn/users_list.zip', 'r')
+users_list = pickle.load(us_zip.open('users_list.pickle'))
+emb_zip = zipfile.ZipFile('service/data/lightFM/emb_maps.zip', 'r')
+embeds_maps = pickle.load(emb_zip.open('emb_maps.pickle'))
 
 
 class Config(BaseSettings):
