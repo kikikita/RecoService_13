c285e593822fe64aaf66a4973c917ac3b9f686fa
kikikita <girmanik.nik@gmail.com>
2022-12-11 00:53:42 +0300

    little fix

--- service/settings.py
+++ service/settings.py
@@ -9,14 +9,11 @@ from pydantic import BaseSettings
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
-zip_knn = zipfile.ZipFile('service/data/knn/knn_bm25_item.zip', 'r')
-knn_model = dill.load(zip_knn.open('knn_bm25_item.dill'))
-zip_pop = zipfile.ZipFile('service/data/knn/pop_model_7.zip', 'r')
-pop_model = dill.load(zip_pop.open('pop_model_7.dill'))
-us_zip = zipfile.ZipFile('service/data/knn/users_list.zip', 'r')
-users_list = pickle.load(us_zip.open('users_list.pickle'))
-emb_zip = zipfile.ZipFile('service/data/lightFM/emb_maps.zip', 'r')
-embeds_maps = pickle.load(emb_zip.open('emb_maps.pickle'))
+zip_data = zipfile.ZipFile('service/data/data.zip', 'r')
+knn_model = dill.load(zip_data.open('knn_bm25_item.dill'))
+pop_model = dill.load(zip_data.open('pop_model_7.dill'))
+users_list = pickle.load(zip_data.open('users_list.pickle'))
+embeds_maps = pickle.load(zip_data.open('emb_maps.pickle'))
 
 
 class Config(BaseSettings):
