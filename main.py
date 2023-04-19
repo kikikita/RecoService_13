f5ca0dbe5302d0c03e4ac4268fa2b6319f31b540
tagirov-am <tagirov.am@list.ru>
2023-04-20 01:32:56 +0300

    sentry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import uvicorn
+import sentry_sdk
 
 from service.api.app import create_app
 from service.settings import get_config
@@ -10,8 +11,12 @@ app = create_app(config)
 
 
 if __name__ == "__main__":
+    sentry_sdk.init(
+        dsn="http://c385d49b389e44f59fe3f96b4f02fe8c@213.202.219.36:9000/1",
+        traces_sample_rate=1.0,
+    )
 
-    host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", "8080"))
+    host = os.getenv("HOST", "213.202.219.36")
+    port = int(os.getenv("PORT", "8002"))
 
     uvicorn.run(app, host=host, port=port)
